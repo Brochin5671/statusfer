bbd114dc46c2537228eb3318cdf56a1fadaf4a63
Brochin5671 <44848459+Brochin5671@users.noreply.github.com>
2020-12-22 18:18:25 -0500

    Added custom messages for register validation

--- validation.js
+++ validation.js
@@ -4,10 +4,23 @@ const joi = require('@hapi/joi');
 // Register validation
 const registerValidation = user => {
     const JoiSchema = joi.object({
-        username: joi.string().trim().required().max(64),
-        email: joi.string().trim().required().insensitive().min(3).max(255).email(),
-        password: joi.string().required().min(8),
-        confirmPassword: joi.any().equal(joi.ref('password')).required().messages({'any.only':'Passwords do not match'})
+        username: joi.string().trim().required().max(64).messages({
+            'string.empty': 'Username is required.',
+            'string.max': 'Username exceeds maximum length of 64 characters.'
+        }),
+        email: joi.string().trim().required().insensitive().min(3).max(255).email().messages({
+            'string.empty': 'Email is required.',
+            'string.min': 'Email must be at least 3 characters long.',
+            'string.max': 'Email exceeds maximum length of 255 characters.',
+            'string.email': 'Email must be valid.'
+        }),
+        password: joi.string().required().min(8).messages({
+            'string.empty': 'Password is required.',
+            'string.min': 'Password must be at least 8 characters long.'
+        }),
+        confirmPassword: joi.any().equal(joi.ref('password')).required().messages({
+            'any.only': 'Passwords do not match.'
+        })
     });
     return JoiSchema.validate(user);
 }
