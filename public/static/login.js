b2cd0b7b67baf2da9d002ba2beda563648cc4b33
Brochin5671 <44848459+Brochin5671@users.noreply.github.com>
2020-12-26 21:39:11 -0500

    Refactored login.js to be more readable

--- public/static/login.js
+++ public/static/login.js
@@ -1,3 +1,11 @@
+// Select reusable elements
+const form = document.getElementById('loginForm');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+
+// Listen for submit event
+form.addEventListener('submit', submitLogin);
+
 // Sends a post request with login form data
 async function submitLogin(event){
     // Prevent refresh
@@ -9,28 +17,24 @@ async function submitLogin(event){
 			'Content-Type': 'application/json'
 		},
 		body: JSON.stringify({
-            'email': form.querySelector('#email').value,
-            'password': form.querySelector('#password').value,
+            'email': emailInput.value,
+            'password': passwordInput.value,
         })
     };
-    const res = await fetch('/user/login',options);
-    const data = await res.json();
-    // Display error tip if failed
-    if(data.error){
+    const res = await fetch('/user/login', options);
+    const {error, message} = await res.json();
+    // Redirect to home on success
+    if(!error){
+        window.location = '/';
+    }else{ // Display error tip on failure
         $('.alert').alert('close');
         const errorDiv = document.createElement('div');
         errorDiv.className = 'alert alert-warning alert-dismissible fade show';
         errorDiv.role = 'alert';
-        errorDiv.innerHTML = data.message+`
+        errorDiv.innerHTML = `${message}
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>`;
-        form.insertBefore(errorDiv, form.firstChild);
-    }else{ // Redirect to home on success
-        window.location = '/';
+        form.firstElementChild.insertAdjacentElement('beforeBegin', errorDiv);
     }
 }
-
-// Listen for submit event
-const form = document.getElementById('loginForm');
-form.addEventListener('submit',submitLogin);
