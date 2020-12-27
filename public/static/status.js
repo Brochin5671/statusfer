a2b6a1dce29c2a6162be0df1a73639476860cda7
Brochin5671 <44848459+Brochin5671@users.noreply.github.com>
2020-12-26 21:52:37 -0500

    Refactored status.js to be more readable

--- public/static/status.js
+++ public/static/status.js
@@ -1,8 +1,13 @@
+// Select username and status text
+const username = document.querySelector('#username');
+const statusText = document.querySelector('#text');
+const statusDate = document.querySelector('#date');
+
 // Get requested status and return it
 async function getStatus(){
     // Get statusId from URL, send get request, and save response
     const statusId = window.location.href.split("/").pop();
-    const res = await fetch('/status/'+statusId+'/data');
+    const res = await fetch(`/status/${statusId}/data`);
     const data = await res.json();
     // Display username and status on success
     if(!data.error){
@@ -11,13 +16,8 @@ async function getStatus(){
         // Get the date of latest revision of the status
         const dateObj = new Date(data.updatedAt);
         statusDate.innerText = dateObj.toString().split(' (')[0];
-    }else{ // Send error
+    }else{ // Send error on failure
         username.innerText = data.error;
         statusText.innerText = data.message;
     }
 }
-
-// Select username and status text
-const username = document.querySelector('#username');
-const statusText = document.querySelector('#text');
-const statusDate = document.querySelector('#date');
