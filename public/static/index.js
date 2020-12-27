a5527a3c66031982e0239f19c99c69376e6651d4
Brochin5671 <44848459+Brochin5671@users.noreply.github.com>
2020-12-26 21:29:15 -0500

    Refactored index.js code to be shorter and more readable

--- public/static/index.js
+++ public/static/index.js
@@ -1,99 +1,97 @@
-// Listen for events
+// Select reusable elements
+const statusList = document.getElementById('statusList');
+const loggedInDiv = document.getElementById('loggedIn');
+const loggedOutDiv = document.getElementById('loggedOut');
 const logoutForm = document.getElementById('logout');
 const statusForm = document.getElementById('statusForm');
 const statusArea = document.getElementById('statusArea');
-logoutForm.addEventListener('click',submitLogout);
-statusForm.addEventListener('submit',postStatus);
-statusArea.addEventListener('input',getTextAreaCharacters);
+
+// Add event listeners
+logoutForm.addEventListener('click', submitLogout);
+statusForm.addEventListener('submit', postStatus);
+statusArea.addEventListener('input', getTextAreaCharacters);
 
 // Get and display all statuses
 async function getStatuses(){
     // Send get request
     const res = await fetch('/status');
-    // Save response, get and fill list with media objects
+    // Save response and fill status list on success
     if(res.status >= 200 && res.status <= 299){
-        const statusList = await res.json();
-        const list = document.querySelector('#statusList');
+        const listJSON = await res.json();
         // Delete inner html contents to refresh list
-        list.innerHTML = '';
-        // Create media objects and append all to list
-        for(let i=0;i<statusList.length;i++){
+        statusList.innerHTML = '';
+        // Create media objects and append all to status list
+        for(let i in listJSON){
             // Create media element
             const statusMedia = document.createElement('li');
-            statusMedia.id = statusList[i]._id;
+            statusMedia.id = listJSON[i]._id;
             // Add border bottom to media element unless last element
-            if(i+1<statusList.length){
-                statusMedia.className = 'media position-relative border-bottom';
-            }else{
-                statusMedia.className = 'media position-relative';
-            }
+            if(i < listJSON.length - 1) statusMedia.className = 'media position-relative border-bottom';
+            else statusMedia.className = 'media position-relative';
             // Create body element
             const statusBody = document.createElement('div');
             statusBody.className = 'media-body m-3 text-break';
             // Create link element
             const statusLink = document.createElement('a');
             statusLink.className = 'btn btn-primary align-self-center mr-3';
-            statusLink.href = '/status/'+statusList[i]._id;
+            statusLink.href = `/status/${listJSON[i]._id}`;
             statusLink.innerText = 'View';
-            // Create user and status text
+            // Create username element
             const username = document.createElement('h5');
-            username.innerText = statusList[i].user;
-            const status = document.createElement('p');
-            status.innerText = statusList[i].status;
-            // Get the date of latest revision of the status and create date text
-            const dateObj = new Date(statusList[i].updatedAt);
-            const date = document.createElement('p');
-            date.className = 'small';
-            date.innerText = dateObj.toString().split(' (')[0];
+            username.innerText = listJSON[i].user;
+            // Create status text element
+            const statusText = document.createElement('p');
+            statusText.innerText = listJSON[i].status;
+            // Get the date of latest revision of the status and create date text element
+            const date = new Date(listJSON[i].updatedAt);
+            const statusDate = document.createElement('p');
+            statusDate.className = 'small';
+            statusDate.innerText = date.toString().split(' (')[0];
             // Append by linking parents
             statusBody.appendChild(username);
-            statusBody.appendChild(status);
-            statusBody.appendChild(date);
+            statusBody.appendChild(statusText);
+            statusBody.appendChild(statusDate);
             statusMedia.appendChild(statusBody);
             statusMedia.appendChild(statusLink);
-            list.appendChild(statusMedia);
+            statusList.appendChild(statusMedia);
             // If status is owned by user, add edit and delete buttons
-            const loggedIn = document.querySelector('#loggedIn h1');
-            if(loggedIn != null && loggedIn.textContent == statusList[i].user){
+            if(loggedInDiv.querySelector('h1').textContent == listJSON[i].user){
                 // Create edit button
                 const editBtn = document.createElement('button');
                 editBtn.innerText = 'Edit';
                 editBtn.type = 'click';
                 editBtn.className = 'btn btn-primary mr-2 edit';
+                editBtn.addEventListener('click', editStatus);
                 statusBody.appendChild(editBtn);
                 // Create delete button
                 const deleteBtn = document.createElement('button');
                 deleteBtn.innerText = 'Delete';
                 deleteBtn.type = 'click';
                 deleteBtn.className = 'btn btn-primary delete';
+                deleteBtn.addEventListener('click', deleteStatus);
                 statusBody.appendChild(deleteBtn);
-                // Add event listeners for edit and delete buttons
-                editBtn.addEventListener('click',editStatus);
-                deleteBtn.addEventListener('click',deleteStatus);
             }
         }
-    }else{ // Send error
+    }else{ // Send error on failure
         alert('Sorry, something went wrong.');
     }
 }
 
-// Get username if logged in
-async function getLoggedInInfo(){
+// Tries to get token to see if user is logged in or out
+async function getLoggedInOrOut(){
     // Send get request with cookies and save response
     const options = {
         method: 'POST',
         credentials: 'same-origin',
     };
-    const res = await fetch('/user/token',options);
-    const data = await res.json();
+    const res = await fetch('/user/token', options);
+    const {error, message} = await res.json();
     // Display loggedIn section and username if logged in
-    if(!data.error){
-        const loggedIn = document.getElementById('loggedIn');
-        loggedIn.className = 'container';
-        loggedIn.querySelector('h1').innerText = data.message;
+    if(!error){
+        loggedInDiv.className = 'container';
+        loggedInDiv.querySelector('h1').innerText = message;
     }else{ // Display loggedOut section if logged out
-        const loggedOut = document.getElementById('loggedOut');
-        loggedOut.className = 'container';
+        loggedOutDiv.className = 'container';
     }
 }
 
@@ -104,7 +102,7 @@ async function submitLogout(event){
         method: 'DELETE',
         credentials: 'same-origin'
     }
-    const res = await fetch('/user/logout',options);
+    const res = await fetch('/user/logout', options);
     // Refresh page on success, else send error
     if(res.status >= 200 && res.status <= 299) window.location.reload();
     else alert('Sorry, something went wrong.');
@@ -123,24 +121,16 @@ async function postStatus(event){
 		},
 		body: statusArea.value
     };
-    const res = await fetch('/status',options);
-    const data = await res.json();
-    // Display error tip if failed
-    if(data.error){
-        $('.alert').alert('close');
-        const errorDiv = document.createElement('div');
-        errorDiv.className = 'alert alert-warning alert-dismissible fade show';
-        errorDiv.role = 'alert';
-        errorDiv.innerHTML = data.message+`
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>`;
-        statusForm.insertBefore(errorDiv, statusForm.firstChild);
-    }else{ // Reset status textarea and character counter, and update status list
+    const res = await fetch('/status', options);
+    const {error, message} = await res.json();
+    // Reset status textarea and character counter, and update status list
+    if(!error){
         statusArea.value = '';
         const charCounter = statusArea.nextElementSibling;
-        charCounter.innerText = '0/255';
+        charCounter.innerText = `0/${statusArea.maxLength}`;
         await getStatuses();
+    }else{ // Display error tip on failure
+        statusForm.firstElementChild.insertAdjacentElement('beforeBegin', createErrorTip(message));
     }
 }
 
@@ -152,22 +142,21 @@ async function editStatus(event){
     textArea.id = 'editedText';
     textArea.rows = 2;
     textArea.maxLength = 255;
-    textArea.addEventListener('input',getTextAreaCharacters);
+    textArea.addEventListener('input', getTextAreaCharacters);
     const statusText = event.composedPath()[1].children[1];
     textArea.value = statusText.innerText;
     statusText.replaceWith(textArea);
     // Create character counter
     const charCounter = document.createElement('p');
     charCounter.className = 'small text-muted mb-2 ml-1';
-    charCounter.innerText = textArea.value.length + '/' + textArea.maxLength;
-    textArea.insertAdjacentElement('afterend', charCounter);
+    charCounter.innerText = `${textArea.value.length}/${textArea.maxLength}`;
+    textArea.insertAdjacentElement('afterEnd', charCounter);
     // Create confirm button and replace edit button
     const confirmBtn = document.createElement('button');
     confirmBtn.innerText = 'Confirm';
     confirmBtn.type = 'click';
     confirmBtn.className = 'btn btn-primary mr-2'; 
-    const editBtn = event.composedPath()[0];
-    editBtn.replaceWith(confirmBtn);
+    event.target.replaceWith(confirmBtn);
     // Add listener to trigger patchStatus()
     confirmBtn.addEventListener('click', patchStatus);
 }
@@ -186,21 +175,13 @@ async function patchStatus(event){
         },
         body: event.composedPath()[1].querySelector('#editedText').value,
     };
-    const res = await fetch('/status/'+statusId,options);
-    const data = await res.json();
-    // Display error tip if failed, else update statuses on success
-    if(data.error){
-        $('.alert').alert('close');
-        const errorDiv = document.createElement('div');
-        errorDiv.className = 'alert alert-warning alert-dismissible fade show';
-        errorDiv.role = 'alert';
-        errorDiv.innerHTML = data.message+`
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>`;
-        statusBody.insertBefore(errorDiv, statusBody.firstChild);
-    }else{
+    const res = await fetch(`/status/${statusId}`, options);
+    const {error, message} = await res.json();
+    // Update statuses on success
+    if(!error){
         await getStatuses();
+    }else{ // Display error tip on failure
+        statusBody.firstElementChild.insertAdjacentElement('beforeBegin', createErrorTip(message));
     }
 }
 
@@ -208,38 +189,44 @@ async function patchStatus(event){
 async function deleteStatus(event){
     // Get status body and status id
     const statusBody = event.composedPath()[1];
-    const statusId = event.toElement.offsetParent.id;
+    const statusId = event.composedPath()[2].id;
     // Send patch request with cookies and form data
     const options = {
         method: 'DELETE',
         credentials: 'same-origin',
     };
-    const res = await fetch('/status/'+statusId,options);
-    const data = await res.json();
-    // Display error tip if failed, else update statuses on success
-    if(data.error){
-        $('.alert').alert('close');
-        const errorDiv = document.createElement('div');
-        errorDiv.className = 'alert alert-warning alert-dismissible fade show';
-        errorDiv.role = 'alert';
-        errorDiv.innerHTML = data.message+`
-            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-            </button>`;
-        statusBody.insertBefore(errorDiv, statusBody.firstChild);
-    }else{
+    const res = await fetch(`/status/${statusId}`, options);
+    const {error, message} = await res.json();
+    // Update statuses on success
+    if(!error){
+        event.target.disabled = true;
         await getStatuses();
+    }else{ // Display error tip on failure
+        statusBody.firstElementChild.insertAdjacentElement('beforeBegin', createErrorTip(message));
     }
+}
+
+// Creates and return error div
+function createErrorTip(message){
+    $('.alert').alert('close');
+    const errorDiv = document.createElement('div');
+    errorDiv.className = 'alert alert-warning alert-dismissible fade show';
+    errorDiv.role = 'alert';
+    errorDiv.innerHTML = `${message}
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+        </button>`;
+    return errorDiv;
 }
 
 // Edits the character counter of a text area
 function getTextAreaCharacters(event){
     const charCounter = event.target.nextElementSibling;
-    charCounter.innerText = event.target.value.length + '/' + event.target.maxLength;
+    charCounter.innerText = `${event.target.value.length}/${event.target.maxLength}`;
 }
 
 // Functions to be executed on load
 function onLoadFunctions(){
-    getLoggedInInfo();
+    getLoggedInOrOut();
     getStatuses();
 }
