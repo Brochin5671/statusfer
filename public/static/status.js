7045338d0a3e248e65fab67d358626f71a66307c
Brochin5671 <44848459+Brochin5671@users.noreply.github.com>
2021-01-04 04:30:32 -0500

    Added socket events for patching and deleting, getting user for menu and modifying status, and auto adding back button

--- public/static/status.js
+++ public/static/status.js
@@ -1,27 +1,70 @@
 // Import
-import {createDateString} from './statusFunctions.js';
+import {getToken} from './requests.js';
+import {createStatusMedia} from './statusFunctions.js';
+import {socket, socketPatchStatus} from './sockets.js';
 
 // Select username and status text
-const username = document.querySelector('#username');
-const statusText = document.querySelector('#text');
-const statusDate = document.querySelector('#date');
+const loggedInDiv = document.getElementById('loggedIn');
+const username = document.getElementById('username');
+const profileLink = document.getElementById('profileLink');
+const statusList = document.getElementById('statusList');
 
-// Get requested status and return it when DOM is loaded
-document.addEventListener("DOMContentLoaded", async () => {
+// Closes any alerts, gets logged in user and requested status
+socket.on('connect', async () => {
+    $('.alert').alert('close');
+    await getUser();
+    await getStatus();
+});
+
+// Listen for socket patchStatus event
+socket.on('patchStatus', socketPatchStatus);
+
+// Listen for socket deleteStatus event
+socket.on('deleteStatus', async () => {
+    await getUser();
+    await getStatus();
+});
+
+// Tries to get token to see if user is logged in or out
+async function getUser(){
+    // Try to get new token
+    const {error, message, userId} = await getToken();
+    // Display loggedIn section and username if logged in
+    if(!error){
+        loggedInDiv.className = 'container';
+        username.innerText = message;
+        profileLink.href = `/user/${userId}`;
+        const span = document.createElement('span');
+        span.className = 'small';
+        span.innerText = ' ▼';
+        username.appendChild(span);
+    }else{ // Hide loggedIn section if logged out and reset elements
+        loggedInDiv.className = 'container d-none';
+        username.innerText = '';
+        profileLink.href = '';
+    }
+}
+
+// Get and display specific status
+async function getStatus(){
     // Get statusId from URL, send get request, and save response
     const statusId = window.location.href.split("/").pop();
     const res = await fetch(`/status/${statusId}/data`);
     const data = await res.json();
     // Display username and status on success
     if(!data.error){
-        username.innerText = data.user;
-        username.href = `../user/${data.userId}`;
-        statusText.innerText = data.status;
-        // Get the date string of the status
-        statusDate.innerText = createDateString(data.createdAt, data.updatedAt);
+        statusList.innerHTML = '';
+        createStatusMedia(data, false);
     }else{ // Send error on failure
-        username.innerText = data.error;
-        username.className += ' text-decoration-none';
+        const statusUser = document.querySelector('.statusUser');
+        statusUser.innerText = data.error;
+        statusUser.className += ' text-decoration-none';
+        const statusText = document.querySelector('.statusText');
         statusText.innerText = data.message;
     }
-});
+    const backBtn = document.createElement('a');
+    backBtn.className = 'btn btn-primary align-self-center mr-3';
+    backBtn.href = '/';
+    backBtn.innerText = 'Back';
+    document.querySelector('.view').replaceWith(backBtn);
+}
