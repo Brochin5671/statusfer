32c9eeb4367f61ef2f62529800fcfe502a16a3f5
Brochin5671 <44848459+Brochin5671@users.noreply.github.com>
2021-01-04 04:32:11 -0500

    Added first main element child check to fix position of error

--- public/static/misc.js
+++ public/static/misc.js
@@ -3,6 +3,8 @@ export function createErrorTip(adjElement, message){
     $('.alert').alert('close');
     const errorDiv = document.createElement('div');
     errorDiv.className = 'alert alert-warning alert-dismissible fade show';
+    // Check if element is to be inserted at the top so it can have a fixed top position
+    if(adjElement === document.querySelector('main').firstElementChild) errorDiv.className += ' fixed-top';
     errorDiv.role = 'alert';
     errorDiv.innerHTML = `${message}
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
