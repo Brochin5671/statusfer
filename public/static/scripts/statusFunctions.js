ba6f4ec299bac929d0c914a81acfcf2c7dd4549f
Brochin5671 <44848459+Brochin5671@users.noreply.github.com>
2021-01-17 01:02:43 -0500

    Updated date string to include months if this year

--- public/static/scripts/statusFunctions.js
+++ public/static/scripts/statusFunctions.js
@@ -101,13 +101,17 @@ export function createDateString(createdAt, updatedAt){
     const now = new Date(Date.now());
     const yesterday = new Date(Date.now());
     yesterday.setDate(now.getDate() - 1);
-    // Check if date is from today or yesterday, else return a full date string
+    // Check if date is from today, yesterday, or this year, else return a full date string
     const isToday = date.getFullYear() == now.getFullYear() && date.getMonth() == now.getMonth() && date.getDate() == now.getDate();
     const isYesterday = date.getFullYear() == yesterday.getFullYear() && date.getMonth() == yesterday.getMonth() && yesterday.getDate() == date.getDate();
+    const isThisYear = date.getFullYear() == now.getFullYear(); 
     if(isToday){
         return `Today at ${date.toLocaleTimeString()}${editDate}`;
     }else if(isYesterday){
         return `Yesterday at ${date.toLocaleTimeString()}${editDate}`;
+    }else if(isThisYear){
+        const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+        return `${months[date.getMonth()]} ${date.getDate()} at ${date.toLocaleTimeString()}${editDate}`;
     }else{
         return `${date.toLocaleDateString()} at ${date.toLocaleTimeString()}${editDate}`;
     }
