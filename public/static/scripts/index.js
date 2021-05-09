01b3481b0b05a725b67588a031c845b9a9beaff9
Brochin5671 <44848459+Brochin5671@users.noreply.github.com>
2021-05-08 23:19:40 -0400

    Added load more button
    Loads 10 posts at a time

--- public/static/scripts/index.js
+++ public/static/scripts/index.js
@@ -4,13 +4,15 @@ import {getToken, submitLogout} from './requests.js';
 import {createStatus, createStatusMedia, getTextAreaCharacters} from './statusFunctions.js';
 import {socket, socketPostStatus, socketDeleteStatus, socketPatchStatus} from './sockets.js';
 
-// Select important elements
+// Important variables
 const statusList = document.getElementById('statusList');
 const userTitle = document.getElementById('userTitle');
 const profileLink = document.getElementById('profileLink');
 const logoutBtn = document.getElementById('logoutBtn');
 const statusForm = document.getElementById('statusForm');
 const statusArea = document.getElementById('statusArea');
+let listJSON = null;
+let currPosts = 0
 
 // Listen for logout event
 logoutBtn.addEventListener('click', submitLogout);
@@ -43,13 +45,23 @@ socket.on('patchStatus', socketPatchStatus);
 async function getStatuses(){
     // Send get request and save response
     const res = await fetch('/status');
-    const listJSON = await res.json();
+    listJSON = await res.json();
     // Fill status list on success
     if(!listJSON.error){
-        // Refresh and append all media objects to status list
+        // Refresh and append 10 media objects to status list
         statusList.innerHTML = '';
-        for(let i in listJSON){
-            createStatusMedia(listJSON[i], false);
+        for(let i = 0; i < listJSON.length && i < 10; i++){
+            createStatusMedia(listJSON[i+currPosts], false);
+        }
+        // If more can be added, add load more button
+        currPosts += 10
+        if(listJSON.length > currPosts){
+            const loadMoreBtn = document.createElement('button');
+            loadMoreBtn.textContent = 'Load More';
+            loadMoreBtn.type = 'click';
+            loadMoreBtn.className = 'btn btn-primary mb-5 shadow';
+            loadMoreBtn.addEventListener('click', loadMoreStatuses);
+            document.getElementById('loadMore').appendChild(loadMoreBtn);
         }
     }else{ // Display error tip on failure
         createErrorTip(document.querySelector('main').firstElementChild, listJSON.message);
@@ -75,5 +87,20 @@ async function getUser(){
         statusArea.value = '';
         const charCounter = statusArea.nextElementSibling;
         charCounter.innerText = `0/${statusArea.maxLength}`;
+    }
+}
+
+// When load more button is clicked, more statuses are added
+function loadMoreStatuses(){
+    // Fill status list with 10 more items
+    if(!listJSON.error){
+        for(let i = currPosts; i < listJSON.length && i < currPosts+10; i++){
+            createStatusMedia(listJSON[i], false);
+        }
+        // If no more can be added, remove load more button
+        currPosts += 10
+        if(listJSON.length <= currPosts){
+            document.getElementById('loadMore').remove();
+        }
     }
 }
