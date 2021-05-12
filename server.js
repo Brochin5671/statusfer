e19cd591639130953675f6b8dc3fabddd60170fa
Brochin5671 <44848459+Brochin5671@users.noreply.github.com>
2021-05-11 20:50:58 -0400

    Changed to common port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // Setup app and port
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 // Setup server and socket.io
 const server = require('http').Server(app);
